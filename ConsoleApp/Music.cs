4c06de1b88c8bc55bb2cd8a2a1aad9b3bb9970c6
p-majchrzak <148012675+p-majchrzak@users.noreply.github.com>
2024-10-02 18:57:41 +0200

    Add project files.

---
+++ ConsoleApp/Music.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    public struct Music
+    {
+        public string artist { get; set; }
+        public string album { get; set; }
+        public int songsNumber  { get; set; }
+        public int year { get; set; }
+        public int downloadNumber { get; set; }
+        public Music(string artist, string album, int songsNumber, int year, int downloadNumber)
+        {
+            this.artist = artist;
+            this.album = album;
+            this.songsNumber = songsNumber;
+            this.year = year;
+            this.downloadNumber = downloadNumber;
+        }
+        
+    }
+}
---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace ConsoleApp
+{
+    internal class Program
+    {
+        public static List<Music> ReadFromFile(string path)
+        {
+            List <Music> musicList = new List <Music> ();
+            StreamReader readFile=new StreamReader(path);
+            while (!readFile.EndOfStream)
+            {
+                string artist = readFile.ReadLine();
+                string album = readFile.ReadLine();
+                int songsNumber = int.Parse(readFile.ReadLine());
+                int year =int.Parse(readFile.ReadLine());
+                int downloadNumber = int.Parse(readFile.ReadLine());
+                string skipLine = readFile.ReadLine();
+                Music music = new Music (artist,album,songsNumber,year,downloadNumber);
+                musicList.Add (music);
+
+            }
+            readFile.Close ();
+            return musicList;
+        }
+        /***********
+        Nazwa funkcji: Display
+        opis funkcji: Funkcja wyswietla dane z listy, ktora zostala przekazana jako parametr funkcji
+        paramtery: list - Parametr typu kolekcji struktury Music
+        zwracany typ i opis: brak
+        Autor: PESEL
+         
+         ***********/
+        public static void Display(List<Music> list)
+        {
+            foreach (Music music in list)
+            {
+                Console.WriteLine(music.artist + "\n" + music.album + "\n" + music.songsNumber + "\n"+ music.year + "\n" + music.downloadNumber + "\n");
+            }
+        }
+        static void Main(string[] args)
+        {
+            Display(ReadFromFile(Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + @"\Data.txt"));
+        }
+    }
+}
